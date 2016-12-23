4133a069abc9709298d94dbc26e5f82eda580906
denis <tlasikee93>
2016-12-23 17:20:20 +0300

    сравнение с однопоточной реализацией

--- src/main/java/ru/tisov/denis/life/Main.java
+++ src/main/java/ru/tisov/denis/life/Main.java
@@ -1,26 +1,36 @@
 package ru.tisov.denis.life;
 
+import ru.tisov.denis.life.base.SimpleLife;
+
 public class Main {
 
     private static final int SIZE = 1000;
-    private static final int GENERATIONS = 3000;
+    private static final int GENERATIONS = 1000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        SimpleLife earth = new SimpleLife(LifeCreator.generate(SIZE));
+        long startTime = System.nanoTime();
+        for (int i = 0; i < GENERATIONS; i++) {
+            earth.nextGeneration();
+        }
+        long baseDuration = System.nanoTime() - startTime;
+        System.out.println("Single thread realization time: " + baseDuration);
+
+
         LifeOrchestrator lifeOrchestrator = new LifeOrchestrator();
 
         Life life = new Life(LifeCreator.generate(SIZE));
 
-        System.out.println(life);
-
-        long startTime = System.nanoTime();
-        for (int gen = 0; gen < GENERATIONS; gen++) {
+        startTime = System.nanoTime();
+        for (int i = 0; i < GENERATIONS; i++) {
             life = lifeOrchestrator.newLife(life);
         }
-        long time = System.nanoTime() - startTime;
 
-        System.out.println(life);
+        long actorsDuration = System.nanoTime() - startTime;
+        System.out.println("Actor's time: " + actorsDuration);
 
-        System.out.println("Time: " + time);
+        System.out.println("Profit: " + ((double) baseDuration / (double) actorsDuration));
 
         lifeOrchestrator.stop();
     }
